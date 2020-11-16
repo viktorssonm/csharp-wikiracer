bc3c5c8daab2c6b6d72ce08258d6a7f2246bfb7e
Magnus Viktorsson <m.viktorsson@gmail.com>
2020-11-16 22:30:49 +0100

    working on getwikipagelinks

--- src/wikiracer/GetWikiPageLinks.cs
+++ src/wikiracer/GetWikiPageLinks.cs
@@ -1,19 +1,24 @@
 using System.Net;
 using System.IO;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 /* 
 Class for fetching all links from a wikipedia page.
 To qualify as a link it must be on the form <a href="/wiki/PAGE_NAME">LINK TEXT</a>
+
+// REGEX <a href="\/wiki\/(.{1,}?)"
 */
 public class GetWikiPageLinks
 {
     public GetWikiPageLinks()
     {
         client = new WebClient();
+        rg = new Regex(REGEX_FOR_WORDS);
     }
 
-    private string getWikipediaSourceForWord(string word)
+    // Get full page source as a string
+    private string GetWikipediaSourceForWord(string word)
     {
 
         Stream data = client.OpenRead($"https://en.wikipedia.org/wiki/{word}");
@@ -22,17 +27,32 @@ public class GetWikiPageLinks
         return s;
     }
 
-    public List<string> getListOfLinksForWord(string word)
+    // Get HashSet of all words in string matching regex. 
+    public HashSet<string> GetListOfLinksForWord(string word)
     {
-        List<string> listOfLinks = new List<string>();
+        HashSet<string> listOfLinks = new HashSet<string>();
+        string page = GetWikipediaSourceForWord(word);
+        var words = rg.Matches(page);
+
+        foreach (Match match in words)
+        {
+            var regexMatch = match.Groups[1].ToString();
+            if (!regexMatch.Contains('#') && !regexMatch.Contains(':'))
+            {
+                listOfLinks.Add(regexMatch);
+            }
+
+        }
+
 
         return listOfLinks;
     }
 
-
-
-
-
     private WebClient client;
     private StreamReader reader;
+    private Regex rg;
+
+    const string REGEX_FOR_WORDS = "<a href=\"\\/wiki\\/(.{1,}?)\"";
 }
+
+
--- src/wikiracer/Program.cs
+++ src/wikiracer/Program.cs
@@ -7,7 +7,7 @@ namespace wikiracer
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
+            GetWikiPageLinks g = new GetWikiPageLinks();
         }
     }
 }
